0d2941bfb3b2062e7b9cd18041d0ffe2da4d9e27
p99kab00 <p99kab00@gmail.com>
2018-10-24 16:05:50 +0300

    Shorthand for link generation added; Record adding implemented

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -2,11 +2,32 @@ class LinksController < ApplicationController
 
     def index
         @links = Link.all
+        @link = Link.new
+    end
+
+    def create
+        @link = Link.new(link_params)
+        @link.shorthand = get_shorthand
+        if @link.save
+            redirect_to root_path
+        else
+            redirect_to 'https://google.com'
+        end
     end
 
     def show
         @link = Link.find_by_shorthand(params[:shorthand])
         redirect_to @link.origin
     end
+
+    private
+        def link_params
+            params.require(:link).permit(:origin)
+        end
+
+        def get_shorthand
+            symbs = [('a'..'z'), ('A'..'Z'), (0..9)].map(&:to_a).flatten
+            (0..7).map { symbs[rand(symbs.length)] }.join
+        end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  root 'links#index'
 
+  root 'links#index'
+  get '/' => 'links#new'
+  post '/' => 'links#create'
   get '/:shorthand' => 'links#show'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
