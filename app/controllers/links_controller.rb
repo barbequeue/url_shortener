529c81dcbd2faadad7fdb7015d6177b664fc8097
p99kab00 <p99kab00@gmail.com>
2018-10-24 14:15:21 +0300

    Redirect to link's origin added

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,6 +1,12 @@
 class LinksController < ApplicationController
 
     def index
+        @links = Link.all
+    end
+
+    def show
+        @link = Link.find_by_shorthand(params[:shorthand])
+        redirect_to @link.origin
     end
 
 end
