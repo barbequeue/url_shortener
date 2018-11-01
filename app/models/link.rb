78c0587d1d167c63b81ccfc0436281c3eb3b6e97
p99kab00 <p99kab00@gmail.com>
2018-11-01 16:51:21 +0200

    URL regex hotfix

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,5 +1,5 @@
 class Link < ApplicationRecord
-    URL_VALIDATION_REGEX = /\A(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?/ix
+    URL_VALIDATION_REGEX = /\A(http|https|)(:\/\/|)[a-zа-я0-9]+([\-\.]{1}[a-zа-я0-9]+)*\.[a-zа-я]{2,5}(:[0-9]{1,5})?(\/.*)?/ix
 
     validates :origin, presence: true, format: { with: URL_VALIDATION_REGEX }
 
